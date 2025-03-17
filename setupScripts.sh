99ec3fcb9bb3035c10c1d16fd1443da99b279e49
Froods <34389217+Froods@users.noreply.github.com>
2025-03-17 21:06:05 +0000

    Change boilerplate in mksite
    Changed the <title> in the index.html file created by mksite, to match site name.

--- setupScripts.sh
+++ setupScripts.sh
@@ -23,7 +23,7 @@ if [ -n "\$1" ]; then
     <head>
         <meta charset=\"UTF-8\">
         <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-        <title>Document</title>
+        <title>\$1</title>
 
         <link rel=\"stylesheet\" type=\"text/css\" href=\"styles/main.css\">
         <script type=\"text/javascript\" src=\"js/js.js\" defer></script>
